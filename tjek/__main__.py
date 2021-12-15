c1873f9018e104649e318d6cae6edc52af2e901b
Vincent <vincentwarmerdam@gmail.com>
2021-12-15 16:08:19 +0100

    base-version

--- tjek/__main__.py
+++ tjek/__main__.py
@@ -4,15 +4,20 @@ import subprocess
 app = typer.Typer(name="tjek", add_completion=False, help="Tjek helps you understand changed git files.")
 
 @app.command()
-def version(name):
+def version():
     """Gives the version of the app"""
     typer.echo(f"0.0.1")
 
 @app.command()
-def diffs(branch: str = typer.Argument("main", help="Branch to compare against.")):
+def diffs(branch: str = typer.Option("origin/main", help="Branch to compare against.")):
     """List updated files compared to another branch."""
-    print(" ".join(["git", "diff", "--name-status", branch]))
-    subprocess.run(["git", "diff", "--name-status", branch, "|", "cat"])
+    typer.echo("\n".join(find_changed_files(branch=branch)))
+    
+def find_changed_files(branch):
+    """Use subprocess to find all files that are different."""
+    output = subprocess.run(["git", "--no-pager", "diff", "--name-status", branch], capture_output=True)
+    files = [s[s.find("\\t")+2:] for s in str(output.stdout).split("\\n")]
+    return [s for s in files if len(s) > 0]
 
 if __name__ == "__main__":
     app()
